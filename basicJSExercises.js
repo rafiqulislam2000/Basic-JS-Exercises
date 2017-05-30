1882b43ad4716a4d5c9e22ddda8e89393b566a6c
rafiqulislam2000 <1520616@iub.edu.bd>
2017-05-31 00:29:41 +0600

    Java Script basics Updated

--- basicJSExercises.js
+++ basicJSExercises.js
@@ -79,14 +79,20 @@ HINT: Use split() and substring()
 
 function protect_email(email) {
 
-  email = emai.split(3,'@')
-  return email;
+  var part1, part2, splited, mid;
+  splited = email.split("@");
+  part1 = splited[0];
+  mid = part1.length / 2;
+  part1 = part1.substring(0, (part1.length - mid));
+  part2 = splited[1];
+
+  return part1 + "....@" + part2;
 }
 
 console.log("Protected email:");
 /* Uncomment the following to check */
-  //console.log(protect_email("harry_potter@gmail.com"));
-  //console.log(protect_email("sarah.connor@gmail.com"));
+  console.log(protect_email("harry_potter@gmail.com"));
+  console.log(protect_email("sarah.connor@gmail.com"));
 
 
 
@@ -106,12 +112,18 @@ HINT: Use join(), split() and sort() functions
  --------------------------- */
 
 function alphabetic_order(word) {
-  return "rearranged word";
+    var arr , splited ,sorted;
+
+      arr = word.split("");
+      arr.sort();
+      sorted=arr.join("");
+
+  return sorted;
 }
 
 console.log("Alphabetic Order:");
 /* Uncomment the following to check */
-  // console.log(alphabetic_order("webmaster"));
+   console.log(alphabetic_order("webmaster"));
 
 
 
@@ -132,11 +144,21 @@ Output:
 
 function remove_duplicates(arr) {
   console.log("Duplicates removed from array");
-}
+    var x, len=arr.length, out=[], obj={};
+ 
+  for (x=0; x<len; x++) {
+    obj[arr[x]]=0;
+  }
+  for (x in obj) {
+    out.push(x);
+  }
+  console.log(out);
+
+   }
 
 console.log("Remove Duplicate Values:");
 /* Uncomment the following to check */
-  // remove_duplicates([3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3]);
+   remove_duplicates([3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3]);
 
 
 /* ---------------------------
@@ -155,6 +177,15 @@ Output:
 
 function dash_in_even(number) {
   console.log("even numbers separated by dashes");
+
+  //var arr;
+  //arr = mumber.split("");
+  //for(var i=0; i<number.length;i++){
+   // if(arr[i]==(i%2)){
+    //  arr = arr.join("-");
+    //}
+ // }
+  //return arr;
 }
 
 console.log("Dash between Even Numbers:");
@@ -176,14 +207,30 @@ the program will display a message "Good Work" otherwise display a message "Not 
 HINT: Use Math.ceil() and Math.random()
 
  --------------------------- */
+  function guessing_game(guess)  
+  {
+ 
+    console.log("matched or unmatched?");
+    var match, num;
+ 
+      num = Math.random() * 100;  
+      num = Math.ceil(num);  
+      num = (num % 10) + 1; 
+      console.log("When guess = " + guess + ", num = " + num);
+ 
+    if(guess === num)
+    {
+      console.log("Good Work");
+    }
 
-function guessing_game(guess) {
-  // Get a random integer from 1 to 10 inclusive
-  console.log("matched or unmatched?");
-}
-
-console.log("Guessing Game:");
-/* Uncomment the following to check */
-  // var guess = prompt('Guess the number between 1 and 10 inclusive');
-  // console.log("User guessed: "+ guess);
-  // guessing_game(guess);
+    else
+    {
+      console.log("Not matched");
+    }
+ 
+  }
+   console.log("Guessing Game:");
+  /* Uncomment the following to check */
+     var guess = prompt('Guess the number between 1 and 10 inclusive');
+     console.log("User guessed: "+ guess);
+     guessing_game(guess);
